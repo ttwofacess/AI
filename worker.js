1f6bde9bb1d05652e0d2df7351a0599e914d0665
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2025-02-06 15:12:47 -0300

    feat: Adding the posibility to choose between diferent models plus correction of errors.

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
-import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
+/* import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm" */
+import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm"
 
 const handler = new WebWorkerMLCEngineHandler()
 
